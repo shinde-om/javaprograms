42124a3430d681984ec29c06b88656cc0f10fc3d
shinde-om <138018972+shinde-om@users.noreply.github.com>
2024-01-01 22:35:22 +0530

    Main final commit

--- CombinationPermutation.java
+++ CombinationPermutation.java
@@ -1,48 +1,65 @@
 package EF138;
 import java.util.Scanner;
-public class CombinationPermutation {
-	static int factorial(int n) {
-		if (n == 1 || n == 0)
+public class CombinationPermutation 
+{
+	static int factorial(int n) 
+	{
+		if (n == 0 || n == 1)
 			return 1;
 		else
-			return factorial(n) * factorial(n-1);
+			return n * factorial(n - 1);
 	}
-	
-	static int nCr(int n, int r) {
-		if(n == 1 || n == 0)
+
+	static int nCr(int n, int r) 
+	{
+		if (r == 0 || r == n)
 			return 1;
 		else
-			return factorial(n)/(factorial(r)*factorial(n-r));
-	}
+			return factorial(n) / (factorial(r) * factorial(n - r));
+	}	
 	
-	static int nPr(int n, int r) {
-		if(n == 0)
+	static int nPr(int n, int r) 
+	{
+		if (r == 0)
 			return 1;
 		else
-			return factorial(n)/factorial(n-r);
+			return factorial(n) / factorial(n - r);
 	}
 	
-	public static void main(String args[]) {
-		System.out.println("Enter option 1/2. 1: for nCr 2: for nPr \n");
-		Scanner sc = new Scanner(System.in);
-		int val = sc.nextInt();
-		System.out.println("Enter the value of n: ");
+	public static void main(String[] args) 
+	{
+		System.out.println("Enter the option 1/2-> 1: for ncr and 2 :for npr");
+		Scanner sc=new Scanner(System.in);
+		int val=sc.nextInt();
+		System.out.print("Enter the value of n: ");
 		int n = sc.nextInt();
-		System.out.println("Enter the value of r: ");
+		System.out.print("Enter the value of r: ");
 		int r = sc.nextInt();
-		switch(val) {
+		switch(val)
+		{
 		case 1:
-			if(n>=r)
-				System.out.println("nCr: " + nCr(n, r));
-			else
-				System.out.println("Invalid input! n should be greater than equal to r!");
+			if (n >= r) 
+			{
+				System.out.println("nCr = " + nCr(n, r));
+			} else 
+			{
+				System.out.println("Invalid input! n should be greater than or equal to r.");
+			}
+			break ;
+ 
 		case 2:
-			if(n>=r)
-				System.out.println("nPr: " + nPr(n, r));
-			else
-				System.out.println("Invalid input! n should be greater than equal to r!");
-		default: System.out.println("Invalid input!");
-		sc.close();
+			if (n >= r) 
+			{
+				System.out.println("nPr = " + nPr(n, r));
+			} else 
+			{
+				System.out.println("Invalid input! n should be greater than or equal to r.");
+			}
+			break ;
+
+		default: System.out.println("Invalid Option"); 
+ 
 		}
+		sc.close();
 	}
 }
